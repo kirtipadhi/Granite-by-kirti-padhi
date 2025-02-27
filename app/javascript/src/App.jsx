b72f217f8305f7c97a9659a0a59da1f0864a7ae2
Kirti Padhi <bigbinary@BigBinarys-MacBook-Pro-4.local>
2025-02-27 17:03:14 +0530

    Implemented create action

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import Dashboard from "./components/Dashboard";
+import Create from "./components/Tasks/Create";
 
 const App = () => (
   <Router>
@@ -10,6 +11,7 @@ const App = () => (
       <Route exact path="/" render={() => <div>Home</div>} />
       <Route exact path="/about" render={() => <div>About</div>} />
       <Route exact component={Dashboard} path="/dashboard" />
+      <Route exact component={Create} path="/tasks/create" />
     </Switch>
   </Router>
 );
